e10c5eb5a3aac046563e91c8379173d9ec17229d
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-08-09 23:00:00 +0200

    Add main page

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutMe from "./AboutMe";
+import Projects from "./Projects";
+
+const Main = () => {
+  return (
+    <main>
+      <AboutMe />
+      <Projects />
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section className="projects">
+      <h2 className="projects_mainTitle">Projects</h2>
+    </section>
+  );
+};
+
+export default Projects;
