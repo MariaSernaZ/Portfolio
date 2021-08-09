ea7da39bcd1cfcc1d867536d23f26510249bccd2
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-08-09 22:35:02 +0200

    Add footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import "../stylesheets/App.scss";
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
