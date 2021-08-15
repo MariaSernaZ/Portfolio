3f248d7f00c290642215585da86613065c85fea7
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-08-15 23:03:16 +0200

    Add nav and cover css

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -5,11 +5,11 @@ const Cover = () => {
   return (
     <>
       <section id="cover" className="cover">
+        <Nav></Nav>
         <div className="coverContainer">
           <h1 className="cover_mainTitle">María Serna Zafra</h1>
           <h2 className="cover_title">Frontend Developer</h2>
         </div>
-        <Nav></Nav>
       </section>
     </>
   );
