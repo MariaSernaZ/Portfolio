7f0a38b336455a3c3c8981ca8d8ad98ea18aae8c
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-08-16 15:56:02 +0200

    Add photo shape footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,38 +2,40 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footerCover footer">
-      <p className="footer_text">Contact</p>
-      <div className="footer_rrss">
-        <ul className="rrss_list">
-          <li className="footerList_item">
-            <a
-              href="https://www.linkedin.com/in/maria-serna-zafra/"
-              alt="Linkedin profile"
-              target="_blank"
-              className="rrss_link">
-              <i class="fab fa-linkedin fa-lg icon"></i>
-            </a>
-          </li>
-          <li className="footerList_item">
-            <a
-              href="https://github.com/MariaSernaZ"
-              alt="Github profile"
-              target="_blank"
-              className="rrss_link">
-              <i class="fab fa-github fa-lg icon"></i>
-            </a>
-          </li>
-          <li className="footerList_item">
-            <a
-              href="mailto:maria.serna.zafra@gmail.com"
-              alt="Email contact"
-              target="_blank"
-              className="rrss_link">
-              <i class="fas fa-envelope-square fa-lg icon"></i>
-            </a>
-          </li>
-        </ul>
+    <footer className="footerCover">
+      <div className="footerContainer">
+        <p className="footer_text">Contact</p>
+        <div className="footer_rrss">
+          <ul className="rrss_list">
+            <li className="footerList_item">
+              <a
+                href="https://www.linkedin.com/in/maria-serna-zafra/"
+                alt="Linkedin profile"
+                target="_blank"
+                className="rrss_link">
+                <i class="fab fa-linkedin fa-lg icon"></i>
+              </a>
+            </li>
+            <li className="footerList_item">
+              <a
+                href="https://github.com/MariaSernaZ"
+                alt="Github profile"
+                target="_blank"
+                className="rrss_link">
+                <i class="fab fa-github fa-lg icon"></i>
+              </a>
+            </li>
+            <li className="footerList_item">
+              <a
+                href="mailto:maria.serna.zafra@gmail.com"
+                alt="Email contact"
+                target="_blank"
+                className="rrss_link">
+                <i class="fas fa-envelope-square fa-lg icon"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
