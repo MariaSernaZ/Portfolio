5581b8dd4a1cd283ba95c92237edd737ea815409
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-08-23 21:13:01 +0200

    Add scss img projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,15 +7,6 @@ function ProjectItem(props) {
       <div>
         <img className="item_img" src={props.img} alt="project photo" />
       </div>
-      <p className="item_description">{props.description}</p>
-      <ul className="item_tags">
-        {props.tags.map((tag, index) => (
-          <li className="item_tag" key={index}>
-            {tag}
-          </li>
-        ))}
-      </ul>
-
       <div className="item_links">
         <a href={props.url} alt={props.title} target="_blank" className="link">
           <i class="fas fa-tv projectIcon"></i>
@@ -27,6 +18,16 @@ function ProjectItem(props) {
           className="link">
           <i class="fas fa-code projectIcon"></i>
         </a>
+      </div>
+      <div className="item_textContainer">
+        <p className="item_description">{props.description}</p>
+        <ul className="item_tags">
+          {props.tags.map((tag, index) => (
+            <li className="item_tag" key={index}>
+              {tag}
+            </li>
+          ))}
+        </ul>
       </div>
     </article>
   );
