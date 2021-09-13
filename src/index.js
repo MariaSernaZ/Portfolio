432685f4d5b6b9191ce16e22912aa747859e845d
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-09-13 14:41:31 +0200

    Add languages library

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./components/App";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+import global_es from "./languages/es/global.json";
+import global_en from "./languages/en/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
