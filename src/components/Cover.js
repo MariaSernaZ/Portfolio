96f6d7b02eddd17f8ba25a7a84711a912e655ded
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-09-14 13:40:14 +0200

    Add cover languages

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,17 +1,31 @@
 import React from "react";
 import Nav from "./Nav.js";
+import { useTranslation } from "react-i18next";
 
 const Cover = () => {
+  const [t, i18n] = useTranslation("global");
   return (
     <>
       <section id="header" className="header">
         <div className="cover">
           <Nav></Nav>
+          <div className="button_language">
+            <button
+              classname="button"
+              onClick={() => i18n.changeLanguage("es")}>
+              ES
+            </button>
+            <button
+              classname="button"
+              onClick={() => i18n.changeLanguage("en")}>
+              EN
+            </button>
+          </div>
         </div>
         <div className="coverText">
           <h1 className="cover_mainTitle">María Serna Zafra</h1>
-          <h2 className="cover_title">&#60;Junior Frontend Developer&#62;</h2>
-          <p className="cover_text">Welcome to my portfolio!</p>
+          <h2 className="cover_title">&#60;{t("cover.job")}&#62;</h2>
+          <p className="cover_text">{t("cover.typewriter")}</p>
         </div>
       </section>
     </>
